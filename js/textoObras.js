79a866fd98c7b679ddc82ec396e8b25b6aac52eb
alberchec <alberchec@hotmail.com>
2022-04-27 20:06:36 -0300

    change text for gallery

--- js/textoObras.js
+++ js/textoObras.js
@@ -1,14 +1,17 @@
-var titgaleria=["","Obra CCP Castelo Branco","Pavimentação Cobreq Indaiatuba","Obra Erca","Obra Exal Itupeva","Obra M3 Jundiaí",
-"Obra Serbom Anhanguera","Obra Bauducco Extrema","Obras de Loteamentos"];
+var titgaleria=["","Base de Piso","Pavimentação","Corte e Aterro","Pavimento Externo","Loteamentos",
+"Drenagem","Demolição","Infraestrutura"];
 
-var textoobra1="Preparo da base dos pisos dos galpões.";
-var textoobra2="Preparo da base do piso dos galpões e pavimentação externa com asfalto.";
-var textoobra3="Preparo da base do piso do galpão e pavimentação externa com bloquetes.";
-var textoobra4="Terraplenagem, drenagem, preparo da base dos pisos dos galpões e pavimentação externa com bloquetes.";
-var textoobra5="Terraplenagem e pavimentação externa com asfalto.";
-var textoobra6="Preparo de base de piso dos galpões para câmara fria.";
-var textoobra7="Preparo da base do piso do galpão.";
-var textoobra8="Infraestrutura e pavimentação.";
+var textoobra1="Preparo de base de pisos de galpões industriais. Nivelamento a laser com \
+excelente planicidade, resultando em redução de matéria prima na confecção do piso de concreto.";
+var textoobra2="Pavimentação de vias, rodovias e áreas externas de galpões industriais.";
+var textoobra3="Disponível o uso de GPS, para melhor precisão e economia nos cortes e aterros.";
+var textoobra4="Preparo da base de pavimentos externos com nivelamento a laser e posterior \
+montagem de bloquetes ou pavimentação.";
+var textoobra5="Solução completa para loteamentos. Desde corte e aterro, drenagem, \
+infraestrutura subterrânea, nivelamento de vias e pavimentação.";
+var textoobra6="Instalação de tubos, confecção de caixas.";
+var textoobra7="Demolição de estruturas de concreto.";
+var textoobra8="Confecção de caixas.";
 
 var txtobras=["", textoobra1, textoobra2, textoobra3, textoobra4, textoobra5, textoobra6, textoobra7, textoobra8];
 
