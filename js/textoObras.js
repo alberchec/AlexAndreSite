0937b7c945d1e499da6c8d13467ee310b5868b9f
alberchec <alberchec@hotmail.com>
2022-04-28 21:30:49 -0300

    change text for gallery

--- js/textoObras.js
+++ js/textoObras.js
@@ -5,13 +5,14 @@ var textoobra1="Preparo de base de pisos de galpões industriais. Nivelamento a 
 excelente planicidade, resultando em redução de matéria prima na confecção do piso de concreto.";
 var textoobra2="Pavimentação de vias, rodovias e áreas externas de galpões industriais.";
 var textoobra3="Disponível o uso de GPS, para melhor precisão e economia nos cortes e aterros.";
-var textoobra4="Preparo da base de pavimentos externos com nivelamento a laser e posterior \
-montagem de bloquetes ou pavimentação.";
+var textoobra4="Preparo da base de pavimentos externos, com nivelamento a laser, para posterior \
+assentamento de bloquetes ou pavimentação.";
 var textoobra5="Solução completa para loteamentos. Desde corte e aterro, drenagem, \
 infraestrutura subterrânea, nivelamento de vias e pavimentação.";
 var textoobra6="Instalação de tubos, confecção de caixas.";
 var textoobra7="Demolição de estruturas de concreto.";
-var textoobra8="Confecção de caixas.";
+var textoobra8="Confecção de caixas para elétrica, esgoto e águas pluviais. Construção de guias,\
+entre outros.";
 
 var txtobras=["", textoobra1, textoobra2, textoobra3, textoobra4, textoobra5, textoobra6, textoobra7, textoobra8];
 
