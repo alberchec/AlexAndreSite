56883370954617f0c29005bf6db4e8f9946f86aa
alberchec <alberchec@hotmail.com>
2022-05-08 03:43:45 -0300

    complete process img file

--- server/py_db/process_new_img.py
+++ server/py_db/process_new_img.py
@@ -1,6 +1,8 @@
 import sqlConnection
 from sqlConnection import connection,read_query
 from shutil import copyfile as cp
+from shutil import move as mv
+from ImageResize import resize
 
 file = open("last_img_added.txt","r")
 id_value = file.readline()
@@ -12,8 +14,17 @@ id_value = int(id_value)
 
 for result in results:
     file = open("last_img_added.txt","w")
-    path = "/var/www/html/images/" + result[1][2:]
+    path = "/var/www/html/piwigo_app/" + result[1][2:]
     filename = result[1][20:]
-    print(str(result[0]) + " " + filename + " " +path)
-    #cp(path,"temp/"+filename)
+    path2 = "/home/alberchec/temp/" + filename
+    pathm = "/var/www/html/images/med-" + filename
+    paths = "/var/www/html/images/sml-" + filename
+    cp(path,path2)
+    resize(path2)
+    cp(path2,pathm)
+    resize(pathm,"-m")
+    cp(path2,paths)
+    resize(paths,"-s")
+    mv(path2,path)
     file.write(str(result[0]) )
+    file.close()
