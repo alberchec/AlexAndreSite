ebf233d8eb566e6f759e33e9fe45b70acafe7ef0
alberchec <alberchec@hotmail.com>
2022-05-08 09:42:35 -0300

    test process_new_img

--- server/py_db/process_new_img.py
+++ server/py_db/process_new_img.py
@@ -4,29 +4,30 @@ from shutil import copyfile as cp
 from shutil import move as mv
 from ImageResize import resize
 
-file = open("last_img_added.txt","r")
-id_value = file.readline()
-file.close()
-
 query = "SELECT id,path FROM piwigo_images WHERE id > " + id_value + " ORDER BY id ASC"
+query = "SELECT id,path,compressed FROM piwigo_images WHERE compressed=0"
 results = read_query(connection,query)
-id_value = int(id_value)
 
-for result in results:
-    path = "/var/www/html/piwigo_app/" + result[1][2:]
-    filename = result[1][20:]
-    path2 = "/home/alberchec/temp/" + filename
-    pathm = "/var/www/html/images/med-" + filename
-    paths = "/var/www/html/images/sml-" + filename
-    cp(path,path2)
-    resize(path2)
-    cp(path2,pathm)
-    resize(pathm,"-m")
-    cp(path2,paths)
-    resize(paths,"-s")
-    mv(path2,path)
-    file = open("last_img_added.txt","w")
-    file.write(str(result[0]) )
-    file.close()
+if results == None:
+    print("None")
+else:
+    print("Not None")
+
+#for result in results:
+#    path = "/var/www/html/piwigo_app/" + result[1][2:]
+#    filename = result[1][20:]
+#    path2 = "/home/alberchec/temp/" + filename
+#    pathm = "/var/www/html/images/med-" + filename
+#    paths = "/var/www/html/images/sml-" + filename
+#    cp(path,path2)
+#    resize(path2)
+#    cp(path2,pathm)
+#    resize(pathm,"-m")
+#    cp(path2,paths)
+#    resize(paths,"-s")
+#    mv(path2,path)
+#    file = open("last_img_added.txt","w")
+#    file.write(str(result[0]) )
+#    file.close()
 
 connection.close()
