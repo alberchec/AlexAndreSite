ee498154cbe99fafb765fd1e9a90202cfef67e63
alberchec <alberchec@hotmail.com>
2022-05-09 15:17:55 -0300

    fix indentation, add comments

--- server/py_db/clean_img.py
+++ server/py_db/clean_img.py
@@ -1,3 +1,7 @@
+##################################################
+###Program intended to remove all images that are
+###not associated with the database.
+##################################################
 import sqlConnection
 from sqlConnection import connection,read_query
 from os import remove
@@ -11,7 +15,7 @@ for file in files:
 	results = read_query(connection,query)
 
 	if results == []:
-    	remove(file)
-    	remove("/var/www/html/images/sml-" + filename)
+		remove(file)
+		remove("/var/www/html/images/sml-" + filename)
 
 connection.close()
