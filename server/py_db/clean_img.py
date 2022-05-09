9302b95b53258c72b0f7262da0bafa0a12933587
alberchec <alberchec@hotmail.com>
2022-05-09 15:08:23 -0300

    add clean img file

---
+++ server/py_db/clean_img.py
@@ -0,0 +1,17 @@
+import sqlConnection
+from sqlConnection import connection,read_query
+from os import remove
+from glob import glob
+
+files = glob("/var/www/html/images/med-*.jpg")
+
+for file in files:
+	filename = file[25:]
+	query = "SELECT id FROM piwigo_images WHERE path LIKE '%" + filename + "'"
+	results = read_query(connection,query)
+
+	if results == []:
+    	remove(file)
+    	remove("/var/www/html/images/sml-" + filename)
+
+connection.close()
